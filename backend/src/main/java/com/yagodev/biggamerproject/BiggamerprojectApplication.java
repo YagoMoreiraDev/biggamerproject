8f2871477279306df5ffe8f31cc9155d1205dbba
Yago Moreira <yagomoreira_dev@yahoo.com>
2024-02-24 20:48:40 -0300

    Iniciando backend

---
+++ backend/src/main/java/com/yagodev/biggamerproject/BiggamerprojectApplication.java
@@ -0,0 +1,13 @@
+package com.yagodev.biggamerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiggamerprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiggamerprojectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yagodev/biggamerproject/BiggamerprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagodev.biggamerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiggamerprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
